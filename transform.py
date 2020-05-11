fd9c97a918e6776cc32ae0f8de6878bc641bb94d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-05-11 12:21:04 -0700

    Validate during transform

--- transform.py
+++ transform.py
@@ -13,6 +13,7 @@ czixml = str((resources / "s_3_t_1_c_3_z_5_meta.xml").resolve(strict=True))
 czisubblock = str((resources / "s_3_t_1_c_3_z_5_subblock.xml").resolve(strict=True))
 template = str((xslt / "czi-to-ome.xsl").resolve(strict=True))
 output = Path("produced.ome.xml").resolve()
+ome_schema = str(Path("ome/ome.xsd").resolve(strict=True))
 
 ###############################################################################
 
@@ -37,9 +38,26 @@ try:
 
 # Catch any exception
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"Error during transform: {e}")
     print("-" * 80)
     print("Full Log:")
     for entry in transform.error_log:
         print((f"{entry.filename}: {entry.line}, "
               f"{entry.column}> {entry.message}>"))
+    raise e
+
+
+# Validate against schema
+try:
+    ome_schema = ET.parse(ome_schema)
+    ome_schema = ET.XMLSchema(ome_schema)
+    ome_schema.assertValid(ome)
+    print("OME XML valid, schema validation ok.")
+
+except ET.DocumentInvalid as e:
+    print("Schema validation error:")
+    raise e
+
+except Exception as e:
+    print("Unknown error:")
+    raise e
