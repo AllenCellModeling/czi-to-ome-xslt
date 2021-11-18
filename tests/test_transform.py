ca91a676a1ad67030e4c2793e9bc356ba8c74049
Matte Bailey <matteb@alleninstitute.org>
2021-11-17 17:18:51 -0800

    Add combined subblocks test file

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -28,6 +28,7 @@ def xslt_path():
     [
         "s_3_t_1_c_3_z_5.czi.xml",
         "s_1_t_1_c_5_z_20.czi.xml",
+        "s_3_t_1_c_3_z_5_with_subblocks.czi.xml",
         "subblocks.czi.xml",
     ],
 )
@@ -49,58 +50,6 @@ def test_transform(xslt_path: str, czi_xml_filename: str):
         # Uncomment this block to look at produced OME XML when manual testing
         # with open("produced.ome.xml", "w") as f:
         #     f.write(ET.tostring(omexml, pretty_print=True, encoding="unicode"))
-
-    # Catch any exception
-    except Exception as e:
-        print("Error during transform")
-        print("-" * 80)
-        print("Full Log:")
-        for entry in transformer.error_log:
-            print(
-                (
-                    f"{entry.filename}: {entry.line}, "
-                    f"{entry.column}> {entry.message}>"
-                )
-            )
-        raise e
-
-    # Test read from OME-Types
-    from_xml(str(omexml))
-
-
-@pytest.mark.parametrize(
-    "czi_xml_filename, czi_subblock_xml_filename",
-    [
-        (
-            "s_3_t_1_c_3_z_5.czi.xml",
-            "s_3_t_1_c_3_z_5.czi.subblock.xml",
-        ),
-    ],
-)
-def test_transform_with_subblock(
-    xslt_path,
-    czi_xml_filename,
-    czi_subblock_xml_filename,
-):
-    # Get full path for CZI XML
-    czi_xml_filepath = str(RESOURCES_DIR / czi_xml_filename)
-    czi_subblock_xml_filepath = str(RESOURCES_DIR / czi_subblock_xml_filename)
-
-    # Parse template and generate transform function
-    template = ET.parse(xslt_path)
-    transformer = ET.XSLT(template)
-
-    # Parse CZI XML
-    czixml = ET.parse(czi_xml_filepath)
-
-    # Add subblock
-    czisubblock = ET.parse(czi_subblock_xml_filepath)
-    czi_root = czixml.getroot()
-    czi_root.append(czisubblock.getroot())
-
-    # Attempt to run transform
-    try:
-        omexml = transformer(czixml)
 
     # Catch any exception
     except Exception as e:
